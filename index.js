f1f9b5fd4c9761f90f5b2eebdd659c2e2be9b7a5
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-06-24 18:50:11 +0300

    if, ternary & switch

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let charges;
+  if (feet <= 400){
+    charges = 'This one is on me!'
+  }
+  else if (feet > 400 && feet < 2000){
+    charges = 'That will be twenty bucks.'
+  }
+  else if (feet > 2000 && feet < 2501){
+    charges = 'I will gladly take your thirty bucks.'
+  }
+  else {
+    charges = 'No can do.'
+  }
+  return charges;
+}
+function ternaryCheckCity(city){
+  let result = city === 'NYC' ? "Ok, sounds good." : "No go.";
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let response;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+  }
+  return response;
 }
